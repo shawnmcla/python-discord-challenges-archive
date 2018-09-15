22dbf7aea520db21273816f86488ca2fe4aaaebe
Shawn McLaughlin <shawnmclau@gmail.com>
2018-09-15 15:17:39 -0400

    Create shawn.py

---
+++ submissions/challenge1/shawn.py
@@ -0,0 +1,17 @@
+"""
+Submission by Shawn
+"""
+class S:
+ def __init__(s):s.s=tuple()
+ def m(s,v):s.s=(*s.s,v)
+ def p(s):t=s.s[-1];s.s=s.s[:-1];return t
+o=lambda z,x,y:{"+":x+y,"−":x-y,"÷":x/(y or 1),"×":x*y}[z]
+def f(i):
+ n=r=0
+ s=S()
+ for c in i:
+  if c.isnumeric():n=n*10+int(c);r=1
+  elif c==' ':
+   if r:s.m(n);n=r=0
+  else:s.m(o(c,s.p(),s.p()))
+ return s.p()
