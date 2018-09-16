7da518abf734bdc646a7337fa3ee0e6622f853c7
Shawn McLaughlin <shawnmclau@gmail.com>
2018-09-16 13:53:12 -0400

    Update grote.py
    updated to support fancy math symbols

--- submissions/challenge1/grote.py
+++ submissions/challenge1/grote.py
@@ -5,4 +5,4 @@ Notes:
 Only works for one operation
 """
 
-lambda t, a={},v=lambda x:(y for y,z in enumerate(x) if z==' '):{*map(lambda b: b.strip().isdigit() and (a.update({len(a):b})or True)or b==" " or  a.update({len(a)-2:str(eval(f"{a.pop(len(a)-1)} {b} {a.pop(len(a)-1)}"))}),(t[a:b]for a,b in zip((0,*v(t)),(*v(t),len(t)))))} and a[0]
+c = lambda t,q=str.replace:(lambda t,a={},d=dict.update,v=lambda x:(y for y,z in enumerate(x)if z==' '):{*map(lambda b: b.strip().isdigit()and(d(a,{len(a):b})or True)or b==" "or d(a,{len(a)-2:str(eval(f"{a.pop(len(a)-1)}{b} {a.pop(len(a)-1)}"))}),(t[a:b]for a,b in zip((0,*v(t)),(*v(t),len(t)))))}and a[0])(q(q(q(t,'×','*'),'−','-'),'÷','/'))
