6ba4593ccfc48d167be100c7f14b9515aed290e7
Joseph <joseph@josephbanks.me>
2018-09-17 21:57:49 +0100

    Update joseph.py

--- submissions/challenge1/joseph.py
+++ submissions/challenge1/joseph.py
@@ -5,4 +5,4 @@ Notes:
 Solution only works on certain environments and requires the 'bc' package to be installed
 """
 
-next(c for c in object.__subclasses__()if'ModuleLock'in str(c)).acquire.__globals__['sys'].modules['os'].execve("dc",("dc","-e",input()+"p"))
+next(c for c in object.__subclasses__()if'ModuleLock'in str(c)).acquire.__globals__['sys'].modules['os'].execvp("dc",("dc","-e",input()+"p"))
